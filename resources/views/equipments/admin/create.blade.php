ce1f90bd103b67508dbfd4a23dd2e30740f4baf0
Akrimi1 <elakrimiiheb@gmail.com>
2022-12-26 15:39:45 +0100

    Fix script

--- resources/views/equipments/admin/create.blade.php
+++ resources/views/equipments/admin/create.blade.php
@@ -38,8 +38,7 @@
       <div class="row mb-2">
          <div class="col-sm-6">
             <h1>Create Product Page</h1>
-         </div>
-         
+         </div>         
       </div>
    </div>
 </section>
