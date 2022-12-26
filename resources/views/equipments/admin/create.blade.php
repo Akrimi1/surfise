214a1883c5f7228ccd15f873b88c431363560939
Akrimi1 <elakrimiiheb@gmail.com>
2022-12-26 21:53:00 +0100

    fix vendor

--- resources/views/equipments/admin/create.blade.php
+++ resources/views/equipments/admin/create.blade.php
@@ -65,22 +65,11 @@
                <div class="col-md-6">
                   <div class="form-group">
                      <label>Add to category</label>
-<<<<<<< HEAD
-                     <select name="idCategory" class="form-control select2" style="width: 100%;">
-                        <option selected="selected">Alabama</option>
-                        <option>Alaska</option>
-                        <option>California</option>
-                        <option>Delaware</option>
-                        <option>Tennessee</option>
-                        <option>Texas</option>
-                        <option>Washington</option>
-=======
                      <select name="idCategory"  id="category" class="form-control select2" style="width: 100%;">
                      @foreach($categories as $c)
                         <option value="{{ $c->id }}">{{ $c->category }}</option>
                         
                      @endforeach
->>>>>>> 61d9beda7cc1c2fc9f9a50a9aaaadf622c6f0722
                      </select>
                      <a href="" class="nav-link" >Add Category</a>
                   </div>
@@ -94,19 +83,8 @@
                <div class="col-md-6">
                   <div class="form-group">
                      <label>Subcategory</label>
-<<<<<<< HEAD
-                     <select name="idSubcategory" class="form-control select2" style="width: 100%;">
-                        <option selected="selected">Alabama</option>
-                        <option>Alaska</option>
-                        <option>California</option>
-                        <option>Delaware</option>
-                        <option>Tennessee</option>
-                        <option>Texas</option>
-                        <option>Washington</option>
-=======
                      <select name="idSubCategory"  id="subcategory" class="form-control select2" style="width: 100%;">
                         <option selected="selected"></option>
->>>>>>> 61d9beda7cc1c2fc9f9a50a9aaaadf622c6f0722
                      </select>
                      <a href="" class="nav-link" >Add Subcategory</a>
                   </div>
@@ -126,23 +104,11 @@
             <div class="row">
                <div class="col-md-6">
                   <div class="form-group">
-<<<<<<< HEAD
-                     <label>Select Category</label>
-                     <select name="idCategory" class="form-control select2" style="width: 100%;">
-                        <option selected="selected">Alabama</option>
-                        <option>Alaska</option>
-                        <option>California</option>
-                        <option>Delaware</option>
-                        <option>Tennessee</option>
-                        <option>Texas</option>
-                        <option>Washington</option>
-=======
                      <label>Add to category</label>
                      <select name="idCategory" class="form-control select2" style="width: 100%;">
                      @foreach($categories as $c)
                         <option value="{{ $c->id }}">{{ $c->category }}</option>
                      @endforeach
->>>>>>> 61d9beda7cc1c2fc9f9a50a9aaaadf622c6f0722
                      </select>
                      <a href="" class="nav-link" >add category</a>
                   </div>
@@ -157,23 +123,10 @@
                   </div>
                </div>
                <div class="col-md-6">
-<<<<<<< HEAD
-                  <div class="form-group">
-                     <label>Select Subcategory</label>
-                     <select name="idSubcategory" class="form-control select2" style="width: 100%;">
-                        <option selected="selected">Alabama</option>
-                        <option>Alaska</option>
-                        <option>California</option>
-                        <option>Delaware</option>
-                        <option>Tennessee</option>
-                        <option>Texas</option>
-                        <option>Washington</option>
-=======
                <div class="form-group">
                      <label>Subcategory</label>
                      <select name="idSubCategory" class="form-control select2" style="width: 100%;">
                         <option selected="selected"></option>
->>>>>>> 61d9beda7cc1c2fc9f9a50a9aaaadf622c6f0722
                      </select>
                      <a href="" class="nav-link" >Add Subcategory</a>
                   </div>
@@ -204,11 +157,7 @@
                <div class="col-md-12 pt-3">
                   <div class="form-group">
                      <label>Description</label>
-<<<<<<< HEAD
                      <textarea name="description" class="form-control" rows="4" placeholder="Enter ..."></textarea>
-=======
-                     <textarea class="form-control" name="description" rows="4" placeholder="Enter ..."></textarea>
->>>>>>> 61d9beda7cc1c2fc9f9a50a9aaaadf622c6f0722
                   </div>
                </div>
                <div class="card-body">
