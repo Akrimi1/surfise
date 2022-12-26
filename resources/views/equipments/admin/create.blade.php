ef4b205725b167df95ceea986477a44754740ba7
Akrimi1 <elakrimiiheb@gmail.com>
2022-12-26 17:06:39 +0100

    product

--- resources/views/equipments/admin/create.blade.php
+++ resources/views/equipments/admin/create.blade.php
@@ -38,8 +38,7 @@
       <div class="row mb-2">
          <div class="col-sm-6">
             <h1>Create Product Page</h1>
-         </div>
-         </div>
+         </div>         
       </div>
    </div>
 </section>
@@ -48,21 +47,29 @@
       <div class="card card-default">
          <div class="card-header">
             <h3 class="card-title"> Quick Add Product</h3>
+            
          </div>
          <div class="card-body">
             <div class="row">
-               <div class="col-md-6">
+            <div class="col-md-6">
                   <div class="form-group">
                      <label>Product type</label>
                      <select class="form-control select2" style="width: 100%;">
                         <option>Accessory</option>
                         <option>Equipment</option>
                         <option>Product</option>
+                      
                      </select>
                   </div>
+                  
+                  
                </div>
+               
                <div class="col-md-6">
+            
                   <div class="form-group">
+           
+                    
                      <label>Add to category</label>
                      <select name="category" class="form-control select2" style="width: 100%;">
                         <option selected="selected">Alabama</option>
@@ -74,15 +81,18 @@
                         <option>Washington</option>
                      </select>
                   </div>
+                  
                </div>
                <div class="col-md-6">
                   <div class="form-group">
                      <label>Enter Product Name</label>
                      <input name="product_name" Placeholder="Enter Product Name" type="text" class="form-control">
                   </div>
+                  
+                  
                </div>
                <div class="col-md-6">
-                  <div class="form-group">
+               <div class="form-group">
                      <label>Subcategory</label>
                      <select name="subcategory" class="form-control select2" style="width: 100%;">
                         <option selected="selected">Alabama</option>
@@ -94,13 +104,14 @@
                         <option>Washington</option>
                      </select>
                   </div>
-               </div>
+            </div>
             </div>
             <hr/>
             <div class="from-group float-right" >
                <button class="btn btn-success">Add +</button>
-            </div>
-         </div>
+            </div> 
+           
+         </div>         
       </div>
       <div class="card card-default">
          <div class="card-header">
@@ -155,14 +166,57 @@
                      <button class="btn btn-primary" >add category</button>
                   </div>
                   <div class="form-group">
-                  <button class="btn btn-primary" onclick="stepper.next()">Next</button>
-                                          
+                     <button class="btn btn-primary" >Add Subcategory</button>
                   </div>
                </div>
             </div>
-            
-         </div>         
-   </div>
+         </div>
+         <div class="card-body">
+            <div class="row">
+               <div class="col-md-6">
+                  <div class="form-group">
+                     <label>brand name</label>
+                     <input type="text" class="form-control">
+                  </div>
+               </div>
+               <div class="col-md-6">
+                  <div class="form-group">
+                     <label>Website</label>
+                     <input type="text" class="form-control">
+                  </div>
+               </div>
+               <div class="from-group">
+                  <label>upload logo</label>
+                  <span class="btn btn-success col fileinput-button">
+                  <i class="fas fa-plus"></i>
+                  <span>Add files</span>
+                  </span>
+                  <button type="reset" class="btn btn-warning col cancel">
+                  <i class="fas fa-times-circle"></i>
+                  <span>Cancel upload</span>
+                  </button>
+               </div>
+            </div>
+            <div class="form-group">
+               <label>Description</label>
+               <textarea class="form-control" rows="4" placeholder="Enter ..."></textarea>
+            </div>
+            <div class="col-md-6">
+               <div class="from-group">
+                  <label>upload other images</label>
+                  <span class="btn btn-success col fileinput-button">
+                  <i class="fas fa-plus"></i>
+                  <span>Add files</span>
+                  </span>
+                  <button type="reset" class="btn btn-warning col cancel">
+                  <i class="fas fa-times-circle"></i>
+                  <span>Cancel upload</span>
+                  </button>
+               </div>
+            </div>
+         </div>
+      </div>
+      
    </div>
 </section>
 @endsection
